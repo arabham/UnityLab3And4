fe7edab3ba70292029f5d324832f7430f84a35e5
Abraham Hoogendoorn <arabham110@gmail.com>
2021-01-31 01:30:43 -0700

    Inital Commit

---
+++ Assets/Scripts/OneBallBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneBallBehaviour : MonoBehaviour
+{
+    public float XSpeed;
+    public float YSpeed;
+    public float ZSpeed; 
+    public float Multiplier = 0.75F;
+    int TooFar = 5;
+
+    static int BallCount = 0;
+    public int BallNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BallCount++;
+        BallNumber = BallCount;
+        ResetBall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Time.deltaTime * XSpeed, Time.deltaTime * YSpeed, Time.deltaTime * ZSpeed);
+        XSpeed += Multiplier - Random.value * Multiplier * 2;
+        YSpeed += Multiplier - Random.value * Multiplier * 2;
+        ZSpeed += Multiplier - Random.value * Multiplier * 2;
+        if ((Mathf.Abs(transform.position.x) > TooFar) || (Mathf.Abs(transform.position.y) > TooFar) || (Mathf.Abs(transform.position.z) > TooFar))
+        {
+            ResetBall();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        GameController controller = Camera.main.GetComponent<GameController>();
+        if (!controller.GameOver)
+        if (!controller.GameOver)
+        {
+            controller.ClickedOnBall();
+            Destroy(gameObject);
+        }
+    }
+
+    void ResetBall()
+    {
+        XSpeed += Multiplier - Random.value * Multiplier * 2;
+        YSpeed += Multiplier - Random.value * Multiplier * 2;
+        ZSpeed += Multiplier - Random.value * Multiplier * 2;
+        transform.position = new Vector3(3 - Random.value * 6, 3 - Random.value * 6, 3 - Random.value * 6);
+    }
+}
